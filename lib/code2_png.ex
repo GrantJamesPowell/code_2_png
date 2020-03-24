ec7373940531b20d2a9dbaca79958784d81d036f
Grant <grant.powell@salesloft.com>
2020-03-23 21:04:22 -0400

    Initial Commit

---
+++ lib/code2_png.ex
@@ -0,0 +1,18 @@
+defmodule Code2Png do
+  @moduledoc """
+  Documentation for `Code2Png`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Code2Png.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/code2_png_test.exs
@@ -0,0 +1,8 @@
+defmodule Code2PngTest do
+  use ExUnit.Case
+  doctest Code2Png
+
+  test "greets the world" do
+    assert Code2Png.hello() == :world
+  end
+end
